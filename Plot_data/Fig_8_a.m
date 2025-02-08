259d98b9c0c3f4ee2f05bad828fba11edab39bd1
ska5623 <98357116+ska5623@users.noreply.github.com>
2025-02-07 22:28:29 -0500

    Delete Plot_data directory

--- Plot_data/Fig_8_a.m
+++
@@ -1,8 +0,0 @@
-figure(1)
-plot(timec,tspringwork,'linewidth',2)
-hold on
-plot(timec,uspringwork,'linewidth',2)
-plot(timec,dspringwork,'linewidth',2)
-legend('thorax spring energy','neg spring energy','pos spring energy');
-xlabel('time (sec)');
-ylabel('energy (J)');
